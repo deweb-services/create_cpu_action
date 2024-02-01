a7b7eda15b680058204b86a4f3d7c811dc7d10ec
Andrei Rybnov <rybnov@dws.sh>
2024-02-01 20:06:21 +0400

    remove brackets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-e="--access_key $1 --secret_key $2 --ssh_key \"$3\" --image \"$4\" --region \"$5\" --cpu $6 --ram $7 --disk_size $8 --disk_type \"$9\" --vpc_id \"${@:13:1}\" --host_name \"${@:14:1}\" --ssh_key_name \"${@:15:1}\""
+e="--access_key $1 --secret_key $2 --ssh_key $3 --image $4 --region $5 --cpu $6 --ram $7 --disk_size $8 --disk_type $9 --vpc_id ${@:13:1} --host_name ${@:14:1} --ssh_key_name ${@:15:1}"
 
 if [ "${@:10:1}" = "true" ]; then
   e+=" --assign_public_ipv4 true"
